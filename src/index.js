92334c326d84293b8bac2e6199aeedf3cee5fe7c
Surafel Kassahun <se.surafel.kassahun@gmail.com>
2022-04-03 11:06:14 +0300

    Fix react 18 deprecated configuration

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import configureStore from './store/configureStore'
-import {login} from './store/user';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import configureStore from "./store/configureStore";
+import { login } from "./store/user";
 
-const store = configureStore()
-store.dispatch(login('papi', 'pass'))
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const store = configureStore();
+store.dispatch(login("papi", "pass"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
